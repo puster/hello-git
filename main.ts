1a56e8e8aac8948db240b490018a75e0fb43356f
puster <puster@gmail.com>
2024-04-17 17:50:04 +0800

    Restore '4/17/2024, 5:43:41 PM Initial files for MakeCode project'

--- main.ts
+++ main.ts
@@ -1,9 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
 })
-input.onButtonPressed(Button.AB, function () {
-    basic.showIcon(IconNames.No)
-})
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Yes)
 })
