e58b91234fec3ef867a6acf4dd9bd3c88dfc832e
puster <puster@gmail.com>
2024-04-20 10:31:18 +0800

    make a clock using 3231

--- main.ts
+++ main.ts
@@ -1,6 +1,18 @@
 input.onButtonPressed(Button.A, function () {
+    DS3231.dateTime(
+    2024,
+    4,
+    20,
+    6,
+    10,
+    12,
+    0
+    )
     basic.showIcon(IconNames.Heart)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Yes)
+    basic.showString("" + convertToText(DS3231.year()) + "/" + convertToText(DS3231.month()) + "/" + convertToText(DS3231.date()))
+})
+loops.everyInterval(2000, function () {
+    basic.showString("" + convertToText(DS3231.hour()) + ":" + convertToText(DS3231.minute()))
 })
